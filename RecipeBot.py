d51a05c389a6c7a7f6de1bfe47c2852cbab39011
Strutio <romeoalrabi@hotmail.com>
2022-03-12 15:53:37 -0500

    Set up connection to the discord client and Reddit API. Added basic client events and other variables to ensure smooth acess of necessary API

--- RecipeBot.py
+++ RecipeBot.py
@@ -1,6 +1,56 @@
 import discord
 import asyncpraw
-from selenium import driver
-from selenium.webdriver.common.by import Keys
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Features to be implemented as time goes on, current placeholder until sprints begin.
+
+# Setting our browser to use Chrome
+driver = webdriver.Chrome()
+
+# Accessing our desired pages to scrape
+# driver.get("https://google.com/")   
+
+# Setting up our connection to discord
+client = discord.Client()
+
+
+# Acquiring access to our Reddit API
+reddit = asyncpraw.Reddit(client_id = 'zxKUahTLCzMsqA', 
+                     client_secret = 'qUTejPPUoIRo4uHZsZTQ7fVrClCLTA',
+                     user_agent = 'Source Collector')
+
+
+# Variables used to check various properties of reddit posts
+
+image_extensions = ['.png', '.jpg', '.jpeg']
+img_title = []
+img_url = []
+img_karma = []
+img_date = []
+img_id = []
+
+
+
+
+# registering events using a callback
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+# Testing bot commands
+
+    if message.content.startswith("!test"):
+        await message.channel.send("Command received!")
+
+
+
+# Running our bot using its dedicated token
+client.run('OTUyMjg0NjE1NTM0NTgzODA4.YizyKA.3fnwxAhIObzZBV_FZ04DsjwSgEM')
