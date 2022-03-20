4440605ac014a25f1cca2e536c110f905e394de5
Rami Alrabi <77370385+Strutio@users.noreply.github.com>
2022-03-20 00:44:55 -0400

    Reworked commands, added cooldowns

--- RecipeBot.py
+++ RecipeBot.py
@@ -3,11 +3,14 @@ import asyncpraw
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from discord.ext import commands
 
 # Features to be implemented as time goes on, current placeholder until sprints begin.
 
 # Setting our browser to use Chrome
 driver = webdriver.Chrome()
+
 
 # Accessing our desired pages to scrape
 # driver.get("https://google.com/")   
@@ -32,6 +35,9 @@ img_date = []
 img_id = []
 
 
+# setting up bot to register commands that start with an !
+bot = commands.Bot(command_prefix='!')
+
 
 
 # registering events using a callback
@@ -45,36 +51,44 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+# if the user triggers a cooldown, this event will return an error message to the user
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        err = 'Please wait 10 seconds between commands'
+        await ctx.send(err)
+
 # Testing bot commands
 
-    if message.content.startswith("!test"):
-        await message.channel.send("Command received!")
+@bot.command()
+async def test(ctx):
+    await ctx.send("Command received!")
 
+# list of commands our bot supports
+list_cmds = ["!ingredients", "!recipes"]
 
-#    if message.content.startswith("!commands"):
- #       await message.channel.send()
+@bot.command()
+async def cmds(ctx):
+    await ctx.send("\n".join(list_cmds))
 
 # gathering what recipe our bot will search for
+@bot.command()
+# adding a cooldown between commands
+@commands.cooldown(1, 10, commands.BucketType.user) 
+async def ingredients(ctx, *, arg):    
 
-    # begins by separating our actual recipe string
-    if message.content.startswith("!ingredients"):
+    # begins by getting access to the recipe site we want to use
+    driver.get('https://www.allrecipes.com/#')
         
-        # gathers the string that we care about following our command
-        content = message.content.split("!ingredients ")[1]
+    # locates the search bar
+    search = driver.find_element(By.XPATH, '//input[@id="search-block"]')
         
-        # begins by getting access to the recipe site we want to use
-        driver.get('https://www.allrecipes.com/#')
+    # inputs the ingredients the user wants into the search bar and searches
+    search.send_keys(arg)
+    search.send_keys(Keys.RETURN)   
         
-        # locates the search bar
-        search = driver.find_element_by_xpath('//input[@id="search-block"]')
-        
-        # inputs the ingredients the user wants into the search bar and searches
-        search.send_keys(content)
-        search.send_keys(Keys.RETURN)
-        await message.channel.send("Found recipes for " + content)
+    await ctx.send("Found recipes for " + arg)
 
-      
-      
-      
-      
-   
+ #TODO
+ # Need a way to randomly retrieve a result, so as to avoid giving the user the same recipe multiple times.
+ # Still possible to randomly get the same recipe twice, but likelihood is lower this way   
